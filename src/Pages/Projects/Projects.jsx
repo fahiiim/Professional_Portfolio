5c6ce6c9b756617696c078b25ecd67d7e7691f9f
Fahim Sarker <fahimsarker0805@gmail.com>
2026-03-30 02:49:26 +0600

    projects

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -55,7 +55,7 @@ const projectsData = [
       'Email notification system integration',
     ],
     tech: ['PyTorch', 'YOLOv8', 'OpenAI API'],
-    image: 'https://images.unsplash.com/photo-1558618666-fcd25c85cd64?w=800&h=600&fit=crop',
+    image: 'https://images.unsplash.com/photo-1713623311317-d3c43a4be4cf?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YWNjaWRlbnR8ZW58MHx8MHx8fDA%3D',
     link: 'https://drive.google.com/file/d/1VOHJBoVYmbouGIb7So12CcpkfN3KCWNn/view',
     linkLabel: 'View Promo',
   },
